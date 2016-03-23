64f99cfa8f4b04baf07875de9abefeb3cf1d70d0
Alexander Korshak <alexskors@gmail.com>
2016-03-23 16:40:43 +0300

    Add missed equals and hashcode

--- mvc_example_1/app/src/main/java/com/mera/mvcweatherchecker/models/WeatherResponse.java
+++ mvc_example_1/app/src/main/java/com/mera/mvcweatherchecker/models/WeatherResponse.java
@@ -22,6 +22,22 @@ public class WeatherResponse {
         return mWeatherData;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WeatherResponse that = (WeatherResponse) o;
+
+        return !(mWeatherData != null ? !mWeatherData.equals(that.mWeatherData) : that.mWeatherData != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return mWeatherData != null ? mWeatherData.hashCode() : 0;
+    }
+
     @Parcel
     public static class WeatherData {
 
@@ -34,12 +50,48 @@ public class WeatherResponse {
 
             @SerializedName("icon")
             String mIcon;
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                Weather weather = (Weather) o;
+
+                if (mDescription != null ? !mDescription.equals(weather.mDescription) : weather.mDescription != null)
+                    return false;
+                return !(mIcon != null ? !mIcon.equals(weather.mIcon) : weather.mIcon != null);
+
+            }
+
+            @Override
+            public int hashCode() {
+                int result = mDescription != null ? mDescription.hashCode() : 0;
+                result = 31 * result + (mIcon != null ? mIcon.hashCode() : 0);
+                return result;
+            }
         }
 
         @Parcel
         private static class Main {
             @SerializedName("temp")
             float mTemp;
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                Main main = (Main) o;
+
+                return Float.compare(main.mTemp, mTemp) == 0;
+
+            }
+
+            @Override
+            public int hashCode() {
+                return (mTemp != +0.0f ? Float.floatToIntBits(mTemp) : 0);
+            }
         }
 
         @SerializedName("main")
@@ -72,6 +124,28 @@ public class WeatherResponse {
                 return mWeather.get(0).mDescription;
             }
             return "";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            WeatherData that = (WeatherData) o;
+
+            if (mMain != null ? !mMain.equals(that.mMain) : that.mMain != null) return false;
+            if (mWeather != null ? !mWeather.equals(that.mWeather) : that.mWeather != null)
+                return false;
+            return !(mCity != null ? !mCity.equals(that.mCity) : that.mCity != null);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = mMain != null ? mMain.hashCode() : 0;
+            result = 31 * result + (mWeather != null ? mWeather.hashCode() : 0);
+            result = 31 * result + (mCity != null ? mCity.hashCode() : 0);
+            return result;
         }
     }
 }
